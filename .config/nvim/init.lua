c89184b2b9d9d2e967ad1021bfc15712597a6e12
Racsok <oscarmcg02@gmail.com>
2025-11-27 15:41:53 -0500

    agregar carpeta nvim/

---
+++ .config/nvim/init.lua
@@ -0,0 +1,13 @@
+--comandos para nvim con lua
+--variables locales
+local vo = vim.o --Configuraciones generales
+local vw = vim.wo --Ámbito de ventana
+local vb = vim.bo --Ámbito de buffer
+
+--varables
+set = vim.opt --Confuguraciones globales globales, de ventana y de buffer
+
+--configuración basica de nvim
+require('basic')
+
+
---
+++ .config/nvim/lua/basic.lua
@@ -0,0 +1,5 @@
+set.nu = true -- muestra la linea de numeros
+set.clipboard = "unnamedplus" --copia al portapapeles del sitema
+set.relativenumber = true 
+set.autoindent = true
+set.autowrite = true
